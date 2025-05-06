2783136b03b77fa18f3599fd6ff70cb8e09e79ab
Manoj Anuragi <61873553+manojanuragi@users.noreply.github.com>
2025-05-06 17:01:23 +0530

    Create ChatWithPdf.jsx

---
+++ src/components/ChatWithPdf.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Chatbot = () => {
+  return (
+    <div style={{ width: '100%', height: '600px' }}>
+      <iframe
+        src="https://manoj1hcl-chatwithpdf.hf.space"
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        title="Chatbot"
+      />
+    </div>
+  );
+};
+
+export default Chatbot;
